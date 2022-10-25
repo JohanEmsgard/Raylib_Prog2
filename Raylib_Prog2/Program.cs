f716689226a9789f326c47d93483df1dfc300500
Johanemsgard <emsgard.johan@elev.ga.ntig.se>
2022-10-25 12:36:49 +0200

    nbcsdbvd

--- Raylib_Prog2/Program.cs
+++ Raylib_Prog2/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using Raylib_cs;
+
+Raylib.InitWindow(900,700, "Bäst");
+Raylib.SetTargetFPS(60);
+
+while (!Raylib.WindowShouldClose())
+{
+
+    //Logik
+    //Grafik
+    Raylib.BeginDrawing();
+    Raylib.ClearBackground(Color.BLUE);
+
+
+
+    Raylib.EndDrawing();
+
+}
+
